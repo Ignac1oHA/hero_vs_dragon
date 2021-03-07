e27421032c427bd4a7666d073a5610b0d92652f7
ihenriquez <ihenriquez86@gmail.com>
2021-03-07 17:42:40 -0300

    Change value to make a stronger Hero.

--- game.py
+++ game.py
@@ -1,36 +1,55 @@
+# This is Ignacio Henríquez homework. Thanks for reading.
+
 import random
+
 # import random is a simple library that implements the random number generator behavior
 
 # Please read into the variables below the correct numbers. Use try and except to catch error.
 # a simple example would be:
+
 # hero_hp = int(input("how many hp does the hero have?"))
 hero_hp = 50
 dragon_hp = 100
-hero_max_dmg = 20
-dragon_max_dmg = 15
+hero_max_dmg = 25
+dragon_max_dmg = 10
 
 print("The dragon with", dragon_hp, "hp attacks out hero with", hero_hp, "hp")
 
 # add a While for an infinite block
 # here goes the while:
+
+while True:
     dragon_attack = random.randint(1, dragon_max_dmg)
-    # here you need to update the hero hp, you need to subtract the damage that the dragon did
-    # add code on this line
+# here you need to update the hero hp, you need to subtract the damage that the dragon did
+# add code on this line
+
+    hero_hp = int(hero_hp - dragon_attack)
 
     print("The dragon does", dragon_attack, "damage and the hero has", hero_hp, "hp left")
-    # add an if condition to check if the hero was killed by the dragon
-    # here goes the if
+
+# add an if condition to check if the hero was killed by the dragon
+# here goes the if
+
+    if hero_hp <= 0:
         print("Unfortunately the dragon killed our hero. RIP sir Bravealot")
         break
+        excepthero_hp >= 1
+        print("Hero is still alive, do not take this for granted!")
 
     hero_attack = random.randint(1, hero_max_dmg)
-    # here you need to update the dragon hp, you need to subtract the damage that the hero did
-    # add code on this line
+# here you need to update the dragon hp, you need to subtract the damage that the hero did
+# add code on this line
+    dragon_hp = (dragon_hp - hero_attack)
 
     print("The hero does", hero_attack, "damage and the dragon has", dragon_hp, "hp left")
     # add an if condition to check if the dragon was killed by the hero
     # here goes the if
+
+    if dragon_hp <= 0:
         print("Our valiant hero has slain the dragon!")
         break
+        excetdragon_hp >= 0
+        print("the dragon is still alive and eager to eat some hobbits.")
 
-    input("Round over. Press any key")
+
+        input("Round over. Press any key")
